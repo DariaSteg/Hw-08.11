3a328b2d87d733353d84c1153ae2fba52ecf73f8
DariaSteg <dariasteg@gmail.com>
2022-11-08 21:45:08 +0200

    Done homework

---
+++ index.js
@@ -0,0 +1,22 @@
+let number = -1;
+let num1 = 7;
+let num2 = 2;
+let num3 = 17;
+
+if (number >= 0){
+    console.log('Positive');}
+else{
+    console.log('Negative');}
+
+
+if (num1 > num2){
+    console.log( num1+=num2 );}
+else{
+    console.log(num1*=num2);}
+
+ if (num3 %7 !== 0  &&  num3 %3 !== 0 ){
+        console.log( "you loose :(" );}
+ else{
+        console.log(" you win! :)");}
+
+
